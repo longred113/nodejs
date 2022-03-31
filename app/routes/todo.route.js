11a3bed94abe3219b90a6689b8afe1d7b7de5dc7
Long <longle35610@gmail.com>
2022-03-31 15:15:03 +0700

    connect mysql

---
+++ app/routes/todo.route.js
@@ -0,0 +1,28 @@
+const todo = require('../controllers/todo.controller');
+var router = require('express').Router();
+module.exports = app => {
+    // Retrieve all todo
+    router.get("/", todo.findAll);
+
+    // Show form create Todo
+    router.get("/create", todo.create);
+    // Store Todo
+    router.post("/", todo.store);
+
+    // Retrieve a single todo with id
+    router.get("/edit/:id", todo.edit);
+    // Update a todo with id
+    router.put("/:id", todo.update);
+
+    // Delete a todo with id
+    router.get("/delete/:id", todo.delete);
+
+    // Delete all todo
+    router.delete("/delete", todo.deleteAll);
+    
+    // Retrieve all published todo
+    router.get("/published", todo.findAllPublished);
+
+    app.use('/todo', router);
+    
+}
--- server.js
+++ server.js
@@ -1,11 +1,38 @@
 const express = require('express');
 const app = express();
+const bodyParser = require('body-parser');
+const expressLayouts = require('express-ejs-layouts');
+const methodOverride = require('method-override');
 require('dotenv').config();
 
-app.get('/', function(req, res){
-    res.send('Hello word')
+app.use(bodyParser.urlencoded({extended: true}));
+app.set('view engine', 'ejs');
+app.set('views','app/views');
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(methodOverride('_method', { methods: ['POST', 'GET'] }));
+app.use(methodOverride(function (req, res) {
+    if (req.body && typeof req.body === 'object' && '_method' in req.body) {
+        var method = req.body._method;
+        delete req.body._method;
+        return method;
+    }
+}));
+
+app.get('/', (req, res, next) => {
+    res.render('index');
+})
+app.get('/500', (req, res) => {
+    res.render('err')
+});
+app.get('/404', (req, res) => {
+    res.render('404')
 });
 
-app.listen(process.env.PORT, function(){
-    console.log('server running: http//localhost:'+process.env.PORT);
+require ('./app/routes/todo.route')(app);
+
+app.listen(3000, function(){
+    console.log('Server running: http//localhost:3000');
 });
+
